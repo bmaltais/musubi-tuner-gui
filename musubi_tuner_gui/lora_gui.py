c1c6460b0b4eb14aeaa7bed80ffe1088357cad5e
bmaltais <bernard@ducourier.com>
2025-01-18 14:06:49 -0500

    Implemented 1st input field

--- musubi_tuner_gui/lora_gui.py
+++ musubi_tuner_gui/lora_gui.py
@@ -1,8 +1,22 @@
 import gradio as gr
+from .class_advanced_training import AdvancedTraining
+from .class_configuration_file import ConfigurationFile
+from .class_gui_config import GUIConfig
 
 def lora_tab(
     headless=False,
+    config: GUIConfig = {},
 ):
     dummy_db_true = gr.Checkbox(value=True, visible=False)
     dummy_db_false = gr.Checkbox(value=False, visible=False)
     dummy_headless = gr.Checkbox(value=headless, visible=False)
+    
+    advanced_training = AdvancedTraining(
+        headless=headless, training_type="lora", config=config
+    )
+    
+    # advanced_training.color_aug.change(
+    #     color_aug_changed,
+    #     inputs=[advanced_training.color_aug],
+    #     outputs=[basic_training.cache_latents],
+    # )
